b1483d54e71d4679ace96b36783817c230ee9c6c
naniprabhakar <55038229+naniprabhakar@users.noreply.github.com>
2019-09-21 15:47:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import RegisterForm from './RegisterForm';
+
+class App extends React.Component {
+  render (){
+    return (
+     
+      <RegisterForm />
+     
+    );
+  }
+}
+
+
+
+export default App;
